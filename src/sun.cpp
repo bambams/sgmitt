f0cf446cd0d77af913d7f3ce4dfbf7c8cdec9e97
Brandon McCaig <bamccaig@gmail.com>
2012-12-23 15:00:37 -0500

    Add copyright notices. sgmitt is now GPLv2+. :)

--- src/sun.cpp
+++ src/sun.cpp
@@ -1,3 +1,23 @@
+/*
+ * sgmitt is a Christmas themed platformer video game.
+ * Copyright (C) 2012 Brandon McCaig
+ *
+ * This file is part of sgmitt.
+ *
+ * sgmitt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * sgmitt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with sgmitt.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "sun.hpp"
 
 H4xSun::H4xSun(
